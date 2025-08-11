e205e145c00de94307ef969a515e67d1921c8acf
OvenTD <155340997+OvenTD@users.noreply.github.com>
2025-08-11 15:47:39 +0900

    1926_2

--- 1926/main.py
+++ 1926/main.py
@@ -5,7 +5,7 @@ canvas = []
 for i in range(n):
     canvas.append(list(map(int, input().split()))) 
 
-def dfs(canvas):
+def bfs(canvas):
     queue = deque()
     n, m = len(canvas), len(canvas[0])
     visited = [ [False]*m for i in range(n)] 
@@ -16,32 +16,31 @@ def dfs(canvas):
         for x, value in enumerate(row):
             if value == 0:
                 continue
-            if visited[y][x] is True:
+            if visited[y][x]:
                 continue
             queue.append((y,x))
             count += 1
             size = 0
             
-            while len(queue) != 0:
+            while queue:
                 qy, qx = queue.popleft()
-                if visited[qy][qx] is True:
-                    continue
-                visited[qy][qx] = True
+
                 size += 1
-                dirs = [[qy+1,qx], [qy-1,qx], [qy,qx+1], [qy,qx-1]]
+                dirs = [(qy+1,qx), (qy-1,qx), (qy,qx+1), (qy,qx-1)]
                 
                 for dir in dirs:
                     dy, dx = dir[0], dir[1]
                     if not (0 <= dy < n and 0 <= dx < m):
                         continue
-                    if visited[dy][dx] is True:
+                    if visited[dy][dx]:
                         continue
                     if canvas[dy][dx] != 1:
                         continue
+                    visited[qy][qx] = True
                     queue.append((dy,dx))
             sizes.append(size)
-    return count, max(sizes)
+    return count, max(sizes) if sizes else 0
 
-p_count, p_size = dfs(canvas)
+p_count, p_size = bfs(canvas)
 print(p_count)
 print(p_size)
