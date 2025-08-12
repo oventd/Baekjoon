17986abc4d62657f621b944e0353a31c75097106
OvenTD <155340997+OvenTD@users.noreply.github.com>
2025-08-12 15:55:14 +0900

    7576_2

--- 7576/main.py
+++ 7576/main.py
@@ -1,43 +1,39 @@
 from collections import deque
-m, n = map(int,input().split())
-boxes = []
-for _ in range(n):
-    row = list(map(int,input().split()))
-    boxes.append(row)
 
+m, n = map(int, input().split())
+boxes = [list(map(int, input().split())) for _ in range(n)]
 
-def bfs(boxes):
+def bfs():
     queue = deque()
+    days = [[0]*m for _ in range(n)]
     visited = [[False]*m for _ in range(n)]
-    dx = [0,0,-1,1]
-    dy = [-1,1,0,0]
-    for y, row in enumerate(boxes):
-        for x, value in enumerate(row):
-            if visited[y][x]:
-                continue
-            if value == 1:
-                visited[y][x] = (y,x,0)
-                queue.append((y,x,0))
-            elif value == -1:
-                visited[y][x] = (y,x,0)
-    while queue:
-        qy, qx, qd = queue.popleft()
-        for i in range(4):
-            ny, nx = qy+dy[i], qx+dx[i]
-            if not (0<=ny<len(boxes) and 0<=nx<len(boxes[0])):
-                continue
-            if visited[ny][nx]:
-                continue
-            if boxes[ny][nx] != 0:
-                continue
-            visited[ny][nx] = (ny,nx,qd+1)
-            queue.append((ny,nx,qd+1))
-    days = []
-    for py in visited:
-        for px in py:
-            if not px:
-                return -1
-            days.append(px[2])
-    return max(days) if days else 0
+    
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
 
-print(bfs(boxes))
+    for y in range(n):
+        for x in range(m):
+            if boxes[y][x] == 1:
+                queue.append((y, x))
+                visited[y][x] = True
+            elif boxes[y][x] == -1:
+                visited[y][x] = True
+    while queue:
+        y, x = queue.popleft()
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+            if 0 <= ny < n and 0 <= nx < m:
+                if not visited[ny][nx] and boxes[ny][nx] == 0:
+                    visited[ny][nx] = True
+                    days[ny][nx] = days[y][x] + 1
+                    queue.append((ny, nx))
+
+    max_day = 0
+    for y in range(n):
+        for x in range(m):
+            if not visited[y][x] and boxes[y][x] == 0:
+                return -1
+            max_day = max(max_day, days[y][x])
+    return max_day
+
+print(bfs())
