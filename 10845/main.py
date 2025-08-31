a14de61e9434b5b87679af89b8ae8e41eae36a71
OvenTD <155340997+OvenTD@users.noreply.github.com>
2025-08-31 21:13:17 +0900

    10845_1

--- 10845/main.py
+++ 10845/main.py
@@ -1,8 +1,12 @@
 from collections import deque
+import sys 
+
+input = sys.stdin.readline 
 
 N = int(input())
 queue = deque()
-for n in range(N):
+
+for _ in range(N):
     usr_input = input()
     if " " in  usr_input:
         command, num = usr_input.split()
@@ -13,24 +17,12 @@ for n in range(N):
     if command == "push":
         queue.append(num)
     elif command == "pop":
-        if len(queue) == 0:
-            print(-1)
-            continue
-        print(queue.popleft())
+        print(queue.popleft() if queue else -1)
     elif command == "size":
         print(len(queue))
     elif command == "empty":
-        if len(queue) ==0:
-            print(1)
-        else:
-            print(0)
+            print(1 if queue else 0)
     elif command == "front":
-        if len(queue) == 0:
-            print(-1)
-            continue
-        print(queue[0])
+        print(queue[0] if queue else -1)
     elif command == "back":
-        if len(queue) == 0:
-            print(-1)
-            continue
-        print(queue[-1])
+        print(queue[-1] if queue else -1)
